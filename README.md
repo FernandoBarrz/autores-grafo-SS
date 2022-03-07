4353477faebeb543af0fbb1079b5bd640ed0ef7b
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-03-06 19:06:49 -0600

    Modifica el README del proyecto

---
+++ README.md
@@ -0,0 +1,8 @@
+# Documentación para la aplicación
+
+> Proyecto creado para el servicio social
+
+
+* A partir de un archivo de texto, extraer los nombres de los autores
+
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+#! global
+
+from ast import Str
+
+
+def my_func(a: str = "aaa", b: int="bbb") -> str:
+    """
+    Print an enriched version of print statement
+    Couse why not?
+    I think this is a better print statements version
+
+    args: 
+        a: A strign containin a string, oh yeah boy
+        b: Another string that creates a string
+
+    return: A great version of the built-in print statement
+
+    """
+    return "A great notice"
+
+my_func()
