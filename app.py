0767cca549805975cd8ec0a6d25b9a78b8a2022d
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-03-18 23:03:17 -0600

    Cambia nombre a setup.py

---
+++ app.py
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+while
+
+
+
+with open('./input/nombre-investigadores.txt', 'r') as nombre_autores:
+    for nombre in nombre_autores:
+        print(nombre)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
