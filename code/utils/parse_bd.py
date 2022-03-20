beb0e5d2db2ec05da3f6a450d45ac5c1b695a07a
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-03-20 13:32:39 -0600

    Completa funcionalidad de parse_bd.py

--- code/utils/parse_bd.py
+++ code/utils/parse_bd.py
@@ -1,26 +1,37 @@
+''' Realiza el <<Parsing>> del archivo .txt
+    Datos aún no listos para realizar operaciones
+'''
 
-with open('../../input/PubMed_Biomedicas_desde_2014.txt', 'r') as publicaciones_bd:
-    raw_publicaciones_bd = list(publicaciones_bd.readlines())
-    bulk_pub_conbo = []
-    temp_pub_single = []
-    for pub in raw_publicaciones_bd:
-        if pub[:2] == 'SO':
-            bulk_pub_conbo.append(temp_pub_single)
-            temp_pub_single = []
-        elif pub != '\n':
-            temp_pub_single.append(pub)
+def parse_txt_to_lists():
+    ''' Genera una lista de listas usando la información del archivo .txt de la BD
+        -> [[a, b], [c, d], [e, f]]
+
+        Parametros:
+        --------------
+        No
+
+        Return:
+        --------------
+        Tipo: List
+        Lista compuesta por listas de públicaciones
+
+    '''
+
+    with open('../../input/PubMed_Biomedicas_desde_2014.txt', 'r') as publicaciones_bd:
+        raw_publicaciones_bd = list(publicaciones_bd.readlines())
+        bulk_pub_conbo = []
+        temp_pub_single = []
+        for pub in raw_publicaciones_bd:
+            if pub[:2] == 'SO':
+                bulk_pub_conbo.append(temp_pub_single)
+                temp_pub_single = []
+            elif pub != '\n':
+                temp_pub_single.append(pub)
+    return bulk_pub_conbo
 
 
-# count_n = 0
-# for publicacion in bulk_pub_conbo:
-#     for part in publicacion:
-#         if 'PMID' in publicacion[1]:
-#             print("OK")
-#         else:
-#             print("fathal error")
-
-
-print(bulk_pub_conbo.__len__())
+bulk = parse_txt_to_lists()
+print(bulk.__len__())
 #print(f"HEEEYYY  -> {count_n}")
 #print(bulk_pub_conbo)
 
