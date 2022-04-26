eefc6d2cb7dd92fb387e79aab823539383644789
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-04-26 00:13:34 -0500

    Refactor option menu in main.py

--- main.py
+++ main.py
@@ -4,53 +4,64 @@ from code.utils.get_names import mostrar_nombre_investigadores
 
 def mostrar_base_datos():
     print('base de datos dasdasd')
+    print('fewfwe\n' * 3)
 def mostrar_documentacion():
     print('documentacion')
+    print('fewfwe\n' * 3)
 def mostrar_grafo():
     print('grafooo')
+    print('fewfwe\n' * 3)
 
 
 
-def main():
-    flag = 1
-    while flag == 1:
-        print('Sistema para mostrar un grafo con peso dirigido que represanta las colaboraciones\n')
-        print('Menú de opciones: \n Escriba un número')
-        opcion_input = input(f'1 - Ver nombre de los investigadores \n2 - mostrar base de datos \n3 - Ver documentación \n4 - Mirar grafo. \n5 / q - Salir del programa \n\nNúmero de opción:\t')
-        if bool(opcion_input) == False:
-            print('Ingrese una opción valida')
-            continue
-        elif opcion_input == '1':
-            os.system('clear || cls')
-            flag = 0
-            mostrar_nombre_investigadores()
-            
 
-        elif opcion_input == '2':
-            mostrar_base_datos()
-            os.system('cls||clear')
+menu_options = {
+    1: 'Ver nombre de los investigadores',
+    2: 'Mostrar base de datos', 
+    3: 'Mostrar grafó',
+    4: 'Salir del programa'
+}
 
-        elif opcion_input == '3':
-            mostrar_documentacion()
-            os.system('cls||clear')
-
-        elif opcion_input == '4':
-            mostrar_grafo()
-            os.system('cls||clear')
-
-        elif opcion_input == 'q' or opcion_input == '5':
-            print("Saliendo del programa...")
-            os.system('cls||clear')
-            flag = 0
-
-        else:
-            print('Escoja una opción valida.')
-            os.system('cls||clear')
-            continue
+def mostrar_menu():
+    print('Sistema para mostrar un grafo con peso no dirigido que represanta las colaboraciones entre investigadores\n')
+    print('Menú de opciones: \n')
+    for key in menu_options.keys():
+        print(key, '--', menu_options[key])
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        mostrar_menu()
+        opcion: str = ''
+        try:
+            opcion = int(input('\nIngrese una opción: \t'))
+        except:
+            print('Error al ingresar una opción')
+
+        if opcion == 1:
+            #os.system('clear || cls')
+            mostrar_nombre_investigadores()
+
+        elif opcion == 2:
+            #os.system('clear || cls')
+            mostrar_base_datos()
+
+        elif opcion == 3:
+            #os.system('clear || cls')
+            mostrar_grafo()
+
+        elif opcion == 4 or opcion == 'q':
+            #os.system('clear || cls')
+            print("Saliendo del programa...")
+            break
+        else:
+            print('Error al seleccionar opción.')
+            os.system('clear || cls')
+            continue
+
+
+
+    
 
 
 
