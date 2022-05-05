1cf1b0b92d0fd23718068b96ff2f0f8acd34a041
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-05-05 15:16:22 -0500

    Add support for spanish on main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import os
 from pprint import pprint
 from code.utils.get_names import mostrar_nombre_investigadores
-from code.utils.parse_bd import print_txt_to_lists, parse_txt_to_lists
-from code.utils.generate_graph import show_graph_cli
+from code.utils.parse_bd import parse_txt_to_lists
+from code.graph.show_graph_cli import show_graph_console
+from code.graph.show_graph_visually import show_graph_image
 from code.utils.generate_statistics import show_statistics_cli
-
-
-
 
 
 menu_options = {
     1: 'Ver nombre de los investigadores',
     2: 'Mostrar base de datos', 
-    3: 'Mostrar grafó',
-    4: 'Mostrar estadísticas',
-    5: 'Salir del programa'
+    3: 'Mostrar grafó (Interfaz de línea de comandos)',
+    4: 'Mostrar grafó (Visualmente al abrir la imagen)',
+    5: 'Mostrar estadísticas',
+    6: 'Salir del programa'
 }
 
 def mostrar_menu():
-    print('Sistema para mostrar un grafo con peso no dirigido que represanta las colaboraciones entre investigadores\n')
+    print('\nSistema para mostrar un grafo con peso no dirigido que represanta las colaboraciones entre investigadores\n')
     print('Menú de opciones: \n')
     for key in menu_options.keys():
         print(key, '--', menu_options[key])
@@ -34,27 +33,29 @@ if __name__ == '__main__':
             print('Error al ingresar una opción')
 
         if opcion == 1:
-            #os.system('clear || cls')
             mostrar_nombre_investigadores()
 
         elif opcion == 2:
-            #os.system('clear || cls')
             bulk = parse_txt_to_lists()
             pprint(bulk)
-            print('Total length: ',len(bulk), end='\n')
+            print('Tamaño total de registros: ',len(bulk), end='\n')
 
         elif opcion == 3:
-            #os.system('clear || cls')
-            show_graph_cli()
+            show_graph_console()
+
         elif opcion == 4:
+            show_graph_image()
+            
+        elif opcion == 5:
             show_statistics_cli()
-        elif opcion == 5 or opcion == 'q':
-            #os.system('clear || cls')
+
+        elif opcion == 6 or opcion == 'q':
             print("Saliendo del programa...")
             break
+
         else:
-            print('Error al seleccionar opción.')
-            os.system('clear || cls')
+            print('\nError al seleccionar opción.')
+            
 
 
 
